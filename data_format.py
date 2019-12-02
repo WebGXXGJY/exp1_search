8e04bdcf6240bdf8e8b0c0d05e2fd8757fc6b406
wanthing <185716272@qq.com>
2019-12-03 00:47:13 +0800

    data_format001

--- data_format.py
+++ data_format.py
@@ -16,39 +16,44 @@ dic = divide.dict_get('文档数据集.csv标题文章分词.txt')
 for index, row in data_relate.iterrows():
     if row['query'] not in query:
         qid += 1
-        row['qid'] = qid
         query.append(row['query'])
+    row['qid'] = qid
     string = str(row['label']) + ' qid:' + str(row['qid'])
     q_word = divide.word_seg(row['query'])
     n_words = len(q_word)
 
     for idx, words in enumerate(q_word):
         if words not in dic['title'].keys():
-            string = string + ' ' + str(idx+1) + ':' + '0'
+            string = string + ' ' + str(idx + 1) + ':' + '0'
+        elif row['doc_id'] not in dic['title'][words].keys():
+            string = string + ' ' + str(idx + 1) + ':' + '0'
         else:
             tf = dic['title'][words][row['doc_id']]
             df = len(dic['title'][words])
             tf_idf = (1 + math.log10(tf)) * (math.log10(n/df))
             string = string + ' ' + str(idx+1) + ':' + str(tf_idf)
-    if n_words + 1 > max_len-1:
+    if n_words > max_len-1:
         print("query中的分词过多")
     else:
-        for i in range(n_words + 1, max_len):
+        for i in range(n_words, max_len):
             string = string + ' ' + str(i+1) + ':' + '0'
 
     for idx, words in enumerate(q_word):
         if words not in dic['content'].keys():
-            string = string + ' ' + str(max_len+idx+1) + ':' + '0'
+            string = string + ' ' + str(max_len + idx + 1) + ':' + '0'
+        elif row['doc_id'] not in dic['content'][words].keys():
+            string = string + ' ' + str(max_len + idx + 1) + ':' + '0'
         else:
             tf = dic['content'][words][row['doc_id']]
             df = len(dic['content'][words])
             tf_idf = (1 + math.log10(tf)) * (math.log10(n/df))
             string = string + ' ' + str(max_len+idx+1) + ':' + str(tf_idf)
-    if n_words + 1 > max_len-1:
+    if n_words > max_len-1:
         print("query中的分词过多")
     else:
-        for i in range(n_words + 1, max_len):
+        for i in range(n_words, max_len):
             string = string + ' ' + str(max_len+i+1) + ':' + '0'
 
     with open("train_data.txt", 'a') as data:
         data.write(string + '\n')
+print('finish')
